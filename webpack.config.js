dd4efe5a63530e73fe4fa52272760fd4e0898a3e
Garryl <g.beckford27@gmail.com>
2026-02-26 00:31:52 -0500

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -15,5 +17,11 @@ module.exports = {
                 loader: 'json-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
